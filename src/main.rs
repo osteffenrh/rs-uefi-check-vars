02bedd1fbd16d704679707cff0f74a53d879357c
Oliver Steffen <osteffen@redhat.com>
2023-01-11 22:08:29 +0100

    Works

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@
 #![no_std]
 #![feature(abi_efiapi)]
 
+extern crate alloc;
+
+use alloc::vec;
+use core::ffi::c_char;
 use log::info;
 use uefi::prelude::*;
 use uefi::proto::device_path::text::{
@@ -10,62 +14,45 @@ use uefi::proto::device_path::text::{
 use uefi::proto::loaded_image::LoadedImage;
 use uefi::table::boot::SearchType;
 use uefi::table::runtime::RuntimeServices;
-use uefi::{Identify, Result};
+use uefi::{CStr16, Identify, prelude, Result};
 use uefi::guid;
-use uefi::CStr16;
+use uefi::CString16;
+
+unsafe fn init(boot_services: &BootServices) {
+    uefi::alloc::init(boot_services);
+}
 
 #[entry]
-fn main(_image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+unsafe fn main(_image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).unwrap();
     let boot_services = system_table.boot_services();
-
-    print_image_path(boot_services).unwrap();
+    init(boot_services);
 
     info!("Hello world!");
+    printvar(system_table.runtime_services());
     system_table.boot_services().stall(10_000_000);
     Status::SUCCESS
 }
 
 fn printvar(runtime_services: &RuntimeServices) {
-    let varname: CStr16 = "Boot0000";
+
+    let n = CString16::try_from("Boot0000").unwrap();
     let guid = uefi::table::runtime::VariableVendor( guid!("8be4df61-93ca-11d2-aa0d-00e098032b8c") );
 
-    let size = runtime_services.get_variable_size(&varname, &guid);
-    info!("varialbe size {}", size);
-    let mut buf: [u8; 64] = ();
-    runtime_services.get_variable(&varname, &guid, &mut buf)?;
-    info!("var = {}", &buf);
+    let size = runtime_services.get_variable_size(n.as_ref(), &guid).expect("Error getting var size");
+    info!("variable size {}", size);
 
-}
+    let mut buf = vec![0u8; size];
+    let _res = runtime_services.get_variable(n.as_ref(), &guid, buf.as_mut_slice()).expect("Error reading var");
 
-fn print_image_path(boot_services: &BootServices) -> Result {
-    let loaded_image = boot_services
-        .open_protocol_exclusive::<LoadedImage>(boot_services.image_handle())?;
-
-    let device_path_to_text_handle = *boot_services
-        .locate_handle_buffer(SearchType::ByProtocol(&DevicePathToText::GUID))?
-        .handles()
-        .first()
-        .expect("DevicePathToText is missing");
-
-    let device_path_to_text = boot_services
-        .open_protocol_exclusive::<DevicePathToText>(
-            device_path_to_text_handle,
-        )?;
-
-    let image_device_path =
-        loaded_image.file_path().expect("File path is not set");
-    let (ptr, size) = loaded_image.info();
-    info!("size: {}", size);
-    let image_device_path_text = device_path_to_text
-        .convert_device_path_to_text(
-            boot_services,
-            image_device_path,
-            DisplayOnly(true),
-            AllowShortcuts(false),
-        )
-        .expect("convert_device_path_to_text failed");
-
-    info!("Image path: {}", &*image_device_path_text);
-    Ok(())
+    let u16v: alloc::vec::Vec<u16> = buf.chunks_exact(2).into_iter()
+        .map(|a| match u16::from_ne_bytes([a[0],a[1]]) {
+            x if x > 64 => x,
+            _ => 0x002Eu16,
+        })
+        .chain([0u16]) // nul terminate
+        .collect();
+    // info!("u16v = {:?}", u16v);
+    let s = CString16::try_from(u16v).expect("Errro converfting");
+    info!("{} = \"{}\"", n, s);
 }
