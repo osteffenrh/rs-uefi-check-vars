9618e4c4f4f590cbe6288c39d566f2083f1af69f
Oliver Steffen <osteffen@redhat.com>
2023-01-11 13:36:12 +0100

    ~wip not working

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,33 @@ use uefi::proto::device_path::text::{
 };
 use uefi::proto::loaded_image::LoadedImage;
 use uefi::table::boot::SearchType;
+use uefi::table::runtime::RuntimeServices;
 use uefi::{Identify, Result};
+use uefi::guid;
+use uefi::CStr16;
 
 #[entry]
-fn main(image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+fn main(_image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).unwrap();
     let boot_services = system_table.boot_services();
 
     print_image_path(boot_services).unwrap();
 
-    boot_services.stall(10_000_000);
+    info!("Hello world!");
+    system_table.boot_services().stall(10_000_000);
     Status::SUCCESS
+}
+
+fn printvar(runtime_services: &RuntimeServices) {
+    let varname: CStr16 = "Boot0000";
+    let guid = uefi::table::runtime::VariableVendor( guid!("8be4df61-93ca-11d2-aa0d-00e098032b8c") );
+
+    let size = runtime_services.get_variable_size(&varname, &guid);
+    info!("varialbe size {}", size);
+    let mut buf: [u8; 64] = ();
+    runtime_services.get_variable(&varname, &guid, &mut buf)?;
+    info!("var = {}", &buf);
+
 }
 
 fn print_image_path(boot_services: &BootServices) -> Result {
